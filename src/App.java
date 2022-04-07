91f32295e0d130a01ad08f4ef2f15aa68d652b61
Darrkhan <darrkhan@protonmail.com>
2022-04-07 09:56:45 +0200

    EPC

--- src/App.java
+++ src/App.java
@@ -1,6 +1,15 @@
+import java.util.*;
 
 public class App{
   public static void main(String[] args){
-    System.out.println("Hello World !");
+    System.out.println("Quel écran souhaitez vous afficher?");
+    System.out.println("1- Ecran prise de commande");
+    System.out.println("2- Ecran cuisine");
+    System.out.println("3- Ecran bar");
+    System.out.println("4- Ecran monitoring");
+
+    Scanner scanner = new Scanner(System.in);
+    int choixEcran = scanner.nextInt();
+    System.out.println("Vous avez choisi l'écran: " + choixEcran);
   }
 }
